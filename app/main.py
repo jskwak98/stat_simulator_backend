a076c80938cbd6b1e87c45a8ecdf1b37a6391938
Jinseong Kwak <narlyeop@sk.com>
2023-12-05 18:59:53 +0900

    ADD: Admin 라우터 추가

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 # main.py
 from fastapi import FastAPI
 from database import engine, metadata, database
-from routers import auth, dice_simul, monty_hall
+from routers import auth, dice_simul, monty_hall, admin
 
 from contextlib import asynccontextmanager
 
@@ -21,3 +21,4 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(auth.router)
 app.include_router(dice_simul.router)
 app.include_router(monty_hall.router)
+app.include_router(admin.router)
