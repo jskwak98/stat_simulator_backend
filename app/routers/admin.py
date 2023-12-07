6a15b7de4ef9275c8d6283e1293a783a41e57233
Jinseong Kwak <narlyeop@sk.com>
2023-12-07 17:04:21 +0900

    ADD : Admin 모든 기능 끄기 추가

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -30,3 +30,13 @@ async def enable_anti_choice(admin_id: str = Query(default="admin", description=
         raise HTTPException(status_code=403, detail="Admin만 통제가 가능해요!")
     simulations_config["anti_choice_enabled"] = True
     return {"message": "숫자 몰래 고르기 기능이 켜졌습니다"}
+
+@router.post("/admin/disable_all")
+async def disable_all(admin_id: str = Query(default="admin", description="Admin ID")):
+    if admin_id != "admin":
+        raise HTTPException(status_code=403, detail="Admin만 통제가 가능해요!")
+    simulations_config["anti_choice_enabled"] = False
+    simulations_config["dice_enabled"] = False
+    simulations_config["monty_hall_enabled"] = False
+    simulations_config["choice_enabled"] = False
+    return {"message": "모든 기능을 껐습니다."}
